6fe00a739ed1fc89732c038d85273bc83a5bb2ae
nil.sanca97 <nil.sanca97@gmail.com>
2026-04-17 09:05:09 +0200

    3rd step translate

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,9 @@
-import { Component, OnInit, signal } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {
+    TranslateService, TranslatePipe,
+    TranslateDirective
+} from "@ngx-translate/core";
 
 @Component({
   selector: 'app-root',
@@ -10,6 +14,13 @@ import { RouterOutlet } from '@angular/router';
 export class App {
   protected readonly title = signal('first-test');
 
+  private translate = inject(TranslateService);
+
   textoParaMostrar = 'Hola soy un contenido dinámico desde angular';
   
+    constructor() {
+        this.translate.addLangs(['es', 'cat', 'en']);
+        this.translate.setFallbackLang('es');
+        this.translate.use('es');
+    }
 }
